23b0c751d1780fea7e1d8a9bf7217de7e97b773e
chengyumeng <792400644@qq.com>
2017-09-24 14:05:11 +0800

    爬虫子模块完成（新增抓取歌词）

---
+++ spider163/spider/lyric.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+import json
+from bs4 import BeautifulSoup
+
+import default
+from spider163 import settings
+from spider163.utils import pysql
+from spider163.utils import pylog
+
+
+class Lyric:
+
+    def __init__(self):
+        self.__headers = default.header
+        self.session = settings.Session()
+
+    def view_lyric(self,song_id):
+        url = default.lyric_url.format(str(song_id))
+        s = requests.session()
+        try:
+            s = BeautifulSoup(s.get(url, headers=self.__headers).content, "html.parser")
+            lrc = json.loads(s.text)['lrc']['lyric']
+            if pysql.single("lyric163","song_id",song_id):
+                self.session.add(pysql.Lyric163(song_id=song_id,txt=lrc))
+                self.session.commit()
+        except:
+            pylog.Log("抓取歌词出现问题，歌曲ID：" + str(song_id))
+
+
+if __name__ == "__main__":
+    tmp = Lyric()
+    tmp.view_lyric(506092019)
---
+++ spider163/spider/playlist.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import default
+import requests
+from bs4 import BeautifulSoup
+
+from spider163.utils import pysql
+from spider163.utils import pylog
+from spider163 import settings
+
+
+class Playlist:
+    __play_url = None
+    __headers = None
+
+    def __init__(self):
+        self.__headers = default.header
+        self.__play_url = default.play_url
+        self.session = settings.Session()
+
+    def view_capture(self, page):
+        s = requests.session()
+        play_url = self.__play_url + str(page * 35)
+        try:
+            acmsk = {'class': 'msk'}
+            scnb = {'class': 'nb'}
+            dcu = {'class': 'u-cover u-cover-1'}
+            ucm = {'class': 'm-cvrlst f-cb'}
+            s = BeautifulSoup(s.get(play_url, headers=self.__headers).content, "html.parser")
+            lst = s.find('ul', ucm)
+            for play in lst.find_all('div', dcu):
+                title = play.find('a', acmsk)['title'].encode('utf-8')
+                link = play.find('a', acmsk)['href'].encode('utf-8').replace("/playlist?id=", "")
+                cnt = play.find('span', scnb).text.encode('utf-8').replace('万', '0000')
+                if pysql.single("playlist163","link",link) == True:
+                    pl = pysql.Playlist163(title=title, link=link, cnt=int(cnt))
+                    self.session.add(pl)
+                    self.session.commit()
+        except:
+            pylog.Log("抓取歌单出现问题，歌单页码：" + page)
+
+
+if __name__ == "__main__":
+    tmp = Playlist()
+    tmp.view_capture(2)
+
