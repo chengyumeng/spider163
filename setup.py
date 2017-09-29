516af8fe2b75f16128c06210cc2cecae623500f9
chengyumeng <792400644@qq.com>
2017-09-29 22:35:06 +0800

    脚本自动执行

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup( name = 'spider163',
-       version = '2.0',
-       author = 'Chengyumeng',
-       url   = 'https://github.com/Chengyumeng/spider163',
-       author_email = 'doublexuan.top@gmail.com',
-       packages = ['spider163']
+setup(name='spider163',
+      author='Chengyumeng',
+      url='https://github.com/Chengyumeng/spider163',
+      author_email='doublexuan.top@gmail.com',
+      packages=find_packages(),
+      scripts=['spider163/bin/spider163'],
 )
