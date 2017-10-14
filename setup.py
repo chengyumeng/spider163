2e63d387b374209ab12c1f9d3162c67ff0f7ba8f
zhangpenghao-pd <zhangpenghao-pd@360.cn>
2017-10-14 17:43:47 +0800

    增加数据库自动创建管理

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
             "requests==2.18.4",
             "shellescape==3.4.1",
             "SQLAlchemy==1.1.14",
+            "SQLAlchemy-Utils==0.32.18",
             "terminaltables==3.1.0",
             "urllib3==1.22",
             "Logbook==1.1.0"
