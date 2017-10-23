b28eb572d380a168960e2e00c7e3ed890acb6abd
ChengTian <792400644@qq.com>
2017-10-23 14:24:23 +0800

    数据统计页面

---
+++ spider163/www/static/js/stat.js
@@ -0,0 +1,89 @@
+$(function () {
+    this.createDom = function () {
+        this.spiderPlaylistObj = $("#spiderPlaylist");
+    }
+    this.documentEvent = function () {
+		var self = this;
+		this.spiderPlaylistObj.click(function() {
+		    var gdType  = $("#gdType").val();
+		    var gdCount = $("#gdCount").val();
+		    $('#gdModal').modal('hide')
+		    $.ajax({
+                url : "/spider/getPlaylist",
+                data:"gdType="+gdType+"&gdCount="+gdCount,
+                type:"post",
+                dataType : "json",
+                success : function (data) {
+                    alert(data["test"]);
+                },
+            });
+		});
+	};
+
+	this.createCharts = function() {
+	     $.ajax({
+                url : "/stat/playlist",
+                type:"get",
+                dataType : "json",
+                success : function (data) {
+                    for (k in data){
+                        var chart = echarts.init(document.getElementById(k), 'macarons');
+                        var t = [];
+                        var v = [];
+                        for (d in data[k]) {
+                            t.push(data[k][d][0]);
+                            v.push(data[k][d][1]);
+                        }
+                        var option = {
+                            title: {text: k},
+                            tooltip: {},
+                            legend: {data:['数量']},
+                            xAxis: {data: t},
+                            yAxis: {},
+                            series: [{name: '数量',type: 'bar',data: v }]
+                            };
+                        chart.setOption(option);
+                    }
+                },
+         });
+
+         $.ajax({
+                url : "/stat/music",
+                type:"get",
+                dataType : "json",
+                success : function (data) {
+                    for (k in data){
+                        var chart = echarts.init(document.getElementById(k), 'macarons');
+                        var t = [];
+                        var v = [];
+                        for (d in data[k]) {
+                            t.push(data[k][d][0]);
+                            v.push(data[k][d][1]);
+                        }
+                        var option = {
+                            title: {text: k},
+                            tooltip: {},
+                            legend: {data:['评论数量']},
+                            xAxis: {data: t,
+                                axisLabel:{
+                                    interval:0,
+                                    rotate:45,//倾斜度 -90 至 90 默认为0
+                                    margin:4,
+                                },
+                            },
+                            yAxis: {},
+                            series: [{name: '评论数量',type: 'bar',data: v }]
+                            };
+                        chart.setOption(option);
+                    }
+                },
+         });
+	}
+	this.init = function() {
+	    this.createDom();
+	    this.documentEvent();
+	    this.createCharts();
+	}
+	this.init()
+
+});
