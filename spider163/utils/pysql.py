1c93b04fa5da4ea8401f8c6dc5e66e2f89c0b836
ChengTian <792400644@qq.com>
2017-11-02 20:26:22 +0800

    扩大表列长度，避免特殊数据报错

--- spider163/utils/pysql.py
+++ spider163/utils/pysql.py
@@ -17,7 +17,7 @@ class Playlist163(Base):
     __tablename__ = "playlist163"
 
     id = Column(Integer(), primary_key=True, autoincrement=True)
-    title = Column(String(255), server_default="System Title")
+    title = Column(String(5000), server_default="System Title")
     link = Column(String(255), server_default="No Link")
     cnt = Column(Integer(), server_default="-1")
     dsc = Column(String(255), server_default="No Description")
@@ -28,10 +28,10 @@ class Playlist163(Base):
 
 class Music163(Base):
     __tablename__ = "music163"
-    id = Column(Integer(),primary_key=True, autoincrement=True)
+    id = Column(Integer(), primary_key=True, autoincrement=True)
     song_id = Column(Integer())
-    song_name = Column(String(300), server_default="No Name")
-    author = Column(String(300), server_default="No Author")
+    song_name = Column(String(5000), server_default="No Name")
+    author = Column(String(5000), server_default="No Author")
     over = Column(String(255), server_default="N")
     has_lyric = Column(String(255), server_default="N")
     create_time = Column(DateTime, server_default=func.now())
@@ -46,7 +46,7 @@ class Comment163(Base):
     id = Column(Integer(), primary_key=True, autoincrement=True)
     song_id = Column(Integer())
     txt = Column(MEDIUMTEXT)
-    author = Column(String(300), server_default="No Author")
+    author = Column(String(5000), server_default="No Author")
     liked = Column(Integer(), server_default="0")
     Index("liked_song_id", liked, song_id)
     Index("song_id_liked", song_id, liked)
