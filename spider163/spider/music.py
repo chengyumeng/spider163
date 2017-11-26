75b3675c6ee4f4f2d6bd825013270f1c78fa751f
Chengyumeng <792400644@qq.com>
2017-11-26 20:48:44 +0800

    增加在线抓取歌单和歌曲的接口

--- spider163/spider/music.py
+++ spider163/spider/music.py
@@ -19,20 +19,28 @@ class Music:
         self.__url = uapi.music_url
         self.session = settings.Session()
 
-    def views_capture(self):
-        urls = self.session.query(pysql.Playlist163).filter(pysql.Playlist163.over == 'N').limit(10)
+    def views_capture(self,source=None):
+        playlist = {}
+        if source is None:
+            urls = self.session.query(pysql.Playlist163).filter(pysql.Playlist163.over == 'N').limit(10)
+        else:
+            if source.startswith("曲风：") is False:
+                source = "曲风：" + source
+            urls = self.session.query(pysql.Playlist163).filter(pysql.Playlist163.over == 'N',pysql.Playlist163.dsc==source).limit(1)
         for url in urls:
             print("正在抓取歌单《{}》的歌曲……".format(url.title.encode("utf-8")))
-            self.view_capture(url.link)
+            songs = self.view_capture(url.link)
+            playlist[url.title.encode("utf-8")] = songs
         for url in urls:
             self.session.query(pysql.Playlist163).filter(pysql.Playlist163.link == url.link).update({'over': 'Y'})
             self.session.commit()
-        return urls.count()
+        return playlist
 
     def view_capture(self, link):
         self.session.query(pysql.Playlist163).filter(pysql.Playlist163.link == link).update({'over': 'Y'})
         url = self.__url + str(link)
         s = requests.session()
+        songs = []
         try:
             s = BeautifulSoup(s.get(url, headers=self.__headers).content, "html.parser")
             musics = json.loads(s.text)['result']['tracks']
@@ -44,9 +52,11 @@ class Music:
                     self.session.add(pysql.Music163(song_id=music['id'],song_name=name,author=author))
                     self.session.commit()
                     exist = exist + 1
+                    songs.append({"name": name,"author": author})
                 else:
                     pylog.log.info('{} : {} {}'.format("重复抓取歌曲", name, "取消持久化"))
             print("歌单包含歌曲 {} 首,数据库 merge 歌曲 {} 首 \r\n".format(len(musics), exist))
+            return songs
         except Exception as e:
             pylog.log.error("抓取歌单页面存在问题：{} 歌单ID：{}".format(e, url))
             raise
--- spider163/www/static/js/spider163.js
+++ spider163/www/static/js/spider163.js
@@ -1,15 +1,20 @@
 $(function () {
     this.createDom = function () {
         this.spiderPlaylistObj = $("#spiderPlaylist");
+        this.SpiderMusicObj    = $("#spiderMusic");
     }
     this.documentEvent = function () {
 		var self = this;
 		$("#gd").click(function(){
 		    $(".gdspider").css("visibility","visible");
 		});
+		$("#gq").click(function(){
+		    $(".gdspider").css("visibility","hidden");
+		    $(".gqspider").css("visibility","visible");
+		});
 		this.spiderPlaylistObj.click(function() {
 		    var gdType  = $("#gdType").val();
-		    var gdPage = $("#gdPage").val();
+		    var gdPage  = $("#gdPage").val();
 		    $.ajax({
                 url : "/spider/getPlaylist",
                 data:"gdType="+gdType+"&gdPage="+gdPage,
@@ -25,6 +30,37 @@ $(function () {
                 },
             });
 		});
+
+		this.SpiderMusicObj.click(function() {
+		    var gdSource = $("#gdSource").val();
+		    var gdCount  = $("#gdCount").val();
+		    for (i=0;i< gdCount; i++ ){
+		        $.ajax({
+                url : "/spider/getMusic",
+                data:"gdSource="+gdSource,
+                type:"post",
+                dataType : "json",
+                success : function (data) {
+                var thead = " <thead><tr><th>#</th><th>歌单名字</th><th>歌曲名字</th><th>作者</th></tr></thead>";
+                var tbody = "";
+                for (playlist in data['data']) {
+                    for ( m in data['data'][playlist]) {
+                        tbody = tbody + "<tr><th scope=\"row\">"
+                        + "</th><td>"+ playlist  +"</td>"
+                        +"<td>"+ data['data'][playlist][m]["name"] + "</td>"
+                        + "<td>"+ data['data'][playlist][m]["author"] + "</td>"
+                        +"</tr>";
+                    }
+                }
+                if((data['data'][playlist]).length > 0) {
+                    $("#printTable").html(thead + "<tbody>" + tbody + "</tbody>");
+                }
+                },
+            });
+		    }
+		});
+
+
 	};
 	this.init = function() {
 	    this.createDom();
