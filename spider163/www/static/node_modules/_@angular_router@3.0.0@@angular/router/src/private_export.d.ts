a73e5ef1ae6ed15c52c507c801a9a287a3cb4273
Chengyumeng <792400644@qq.com>
2017-12-02 15:36:47 +0800

    删除angular依赖（小项目无须angular库、）

--- spider163/www/static/node_modules/_@angular_router@3.0.0@@angular/router/src/private_export.d.ts
+++
@@ -1,8 +0,0 @@
-import { ROUTES } from './router_config_loader';
-import { ROUTER_PROVIDERS } from './router_module';
-import { flatten } from './utils/collection';
-export declare var __router_private__: {
-    ROUTER_PROVIDERS: typeof ROUTER_PROVIDERS;
-    ROUTES: typeof ROUTES;
-    flatten: typeof flatten;
-};
