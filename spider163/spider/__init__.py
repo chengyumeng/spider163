fec81d6ab39fc33d9ab85811d9c91316fc6a7bcd
chengyumeng <792400644@qq.com>
2017-12-29 09:31:12 +0800

    增加下载歌曲提示，忽略数据库问题

--- spider163/spider/__init__.py
+++ spider163/spider/__init__.py
@@ -1,3 +1,9 @@
-from spider163 import settings
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-settings.configure_orm()
+from spider163 import settings
+from spider163.utils import pylog
+try:
+    settings.configure_orm()
+except Exception as e:
+    pylog.print_info("无法执行数据库相关的任务： {}".format(e))
