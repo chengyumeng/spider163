22250f2f8e06c0c458bc830a9e320e21feb4e2ea
chengyumeng <792400644@qq.com>
2018-02-14 23:43:43 +0800

    修复抓取歌单歌曲异常条件下不能完成正确存储的问题

--- spider163/spider/music.py
+++ spider163/spider/music.py
@@ -30,13 +30,9 @@ class Music:
             print("正在抓取歌单《{}》的歌曲……".format(tools.encode(url.title)))
             songs = self.view_capture(url.link)
             playlist[tools.encode(url.title)] = songs
-        for url in urls:
-            self.session.query(pysql.Playlist163).filter(pysql.Playlist163.link == url.link).update({'done': 'Y'})
-            self.session.commit()
         return playlist
 
     def view_capture(self, link):
-        self.session.query(pysql.Playlist163).filter(pysql.Playlist163.link == link).update({'done': 'Y'})
         url = self.__url + str(link)
         songs = []
         try:
@@ -46,7 +42,10 @@ class Music:
             for music in musics:
                 name = tools.encode(music['name'])
                 author = tools.encode(music['artists'][0]['name'])
-                play_time = music["bMusic"]["playTime"]
+                if music["bMusic"] is None:
+                    play_time = 0
+                else:
+                    play_time = music["bMusic"]["playTime"]
                 if pysql.single("music163", "song_id", (music['id'])) is True:
                     self.session.add(pysql.Music163(song_id=music['id'],song_name=name,author=author,playTime=play_time))
                     self.session.commit()
@@ -55,6 +54,8 @@ class Music:
                 else:
                     pylog.log.info('{} : {} {}'.format("重复抓取歌曲", name, "取消持久化"))
             print("歌单包含歌曲 {} 首,数据库 merge 歌曲 {} 首 \r\n".format(len(musics), exist))
+            self.session.query(pysql.Playlist163).filter(pysql.Playlist163.link == link).update({'done': 'Y'})
+            self.session.commit()
             return songs
         except Exception as e:
             pylog.log.error("抓取歌单页面存在问题：{} 歌单ID：{}".format(e, url))
@@ -64,12 +65,22 @@ class Music:
 
     def curl_playlist(self,playlist_id):
         url = uapi.playlist_api.format(playlist_id)
-        data = tools.curl(url, self.__headers)
-        playlist = data['result']
-        self.session.query(pysql.Playlist163).\
-            filter(pysql.Playlist163.link == playlist_id).\
-            update({"playCount": playlist["playCount"], "shareCount": playlist["shareCount"],"commentCount": playlist["commentCount"],"description":playlist["description"],"tags":",".join(playlist["tags"])})
-        return playlist
+        try:
+            data = tools.curl(url, self.__headers)
+            playlist = data['result']
+            self.session.query(pysql.Playlist163).\
+                filter(pysql.Playlist163.link == playlist_id).\
+                update({"playCount": playlist["playCount"],
+                    "shareCount": playlist["shareCount"],
+                    "commentCount": playlist["commentCount"],
+                    "description": playlist["description"],
+                    "tags":",".join(playlist["tags"])})
+            return playlist
+        except Exception as e:
+            pylog.Log("抓取歌单页面存在问题：{} 歌单ID：{}".format(e, playlist_id))
+            # pylog.print_warn("抓取歌单页面存在问题：{} 歌单ID：{}".format(e, playlist_id))
+            self.session.query(pysql.Playlist163).filter(pysql.Playlist163.link == playlist_id).update({'done': 'E'})
+            self.session.commit()
 
     def get_playlist(self, playlist_id):
         self.view_capture(int(playlist_id))
