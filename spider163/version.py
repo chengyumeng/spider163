715ae30affb6aceb79eb60d507525d5610512878
chengyumeng <792400644@qq.com>
2018-04-07 21:16:33 +0800

    增加邮件模板文件

--- spider163/version.py
+++ spider163/version.py
@@ -35,3 +35,25 @@ No.4 通过微信向作者转账赞助。
 
 """
 root_path = os.path.dirname(os.path.abspath(__file__))
+
+MAILBODY = """
+<h2 style="color: #C20C0C; margin: 10px 0;"><a href="https://github.com/Chengyumeng/spider163" target="_blank">Spider163</a> 云音乐今日精彩推荐(微信公众号：pod1024)</h2>
+<h2 style="color: #C20C0C; margin: 10px 0;"></h2>
+<p  style="color: #C10B0B; margin: 10px 0;" >今日分享评论数量排行 {} - {} 歌曲：</p>
+<ul>{}</ul>
+<div style="margin: 20px 0 0 0;">
+<p style="font-weight: 400;font-style: normal;font-size: 30px;color: #333;text-align: center;margin: 30px auto;">欢迎关注程天写代码微信公众号：pod1024</p>
+</div>
+"""
+
+MAILMUSIC = """
+<li><span style="font-weight: bold; margin: 2px 10px 5px 10px;"><a href="http://music.163.com/#/song?id={}" target="_blank">{}</a></span>
+<span style="font-weight: bold; margin: 2px 10px 5px 10px;">{}</span> 
+<span style="font-weight: bold; margin: 2px 10px 5px 10px;">评论数：{}</span></li><hr>
+{}
+"""
+MAILCOMMENT = """
+<p><span style="font-weight: bold; margin: 2px 10px 5px 10px;color: #a40011;">{}</span> 
+<span style="font-weight: bold; margin: 2px 10px 5px 10px;color:">{} :</span> </p>
+<p style="margin: 12px 20px 15px 20px;">{}</p>
+"""
