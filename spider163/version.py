c1b879e1edc5fcb7cc9cdca18a2c2e3a0ccaf103
chengyumeng <792400644@qq.com>
2018-04-14 18:33:55 +0800

    调整邮件结构

--- spider163/version.py
+++ spider163/version.py
@@ -39,7 +39,7 @@ root_path = os.path.dirname(os.path.abspath(__file__))
 MAILBODY = """
 <h2 style="color: #C20C0C; margin: 10px 0;"><a href="https://github.com/Chengyumeng/spider163" target="_blank">Spider163</a> 云音乐今日精彩推荐(微信公众号：pod1024)</h2>
 <h2 style="color: #C20C0C; margin: 10px 0;"></h2>
-<p  style="color: #C10B0B; margin: 10px 0;" >今日分享评论数量排行 {} - {} 歌曲：</p>
+<p  style="color: #C10B0B; margin: 10px 0;" >今日分享 {} 摘编歌曲：</p>
 <ul>{}</ul>
 <div style="margin: 20px 0 0 0;">
 <p style="font-weight: 400;font-style: normal;font-size: 30px;color: #333;text-align: center;margin: 30px auto;">欢迎关注程天写代码微信公众号：pod1024</p>
@@ -52,8 +52,9 @@ MAILMUSIC = """
 <span style="font-weight: bold; margin: 2px 10px 5px 10px;">评论数：{}</span></li><hr>
 {}
 """
+
 MAILCOMMENT = """
-<p><span style="font-weight: bold; margin: 2px 10px 5px 10px;color: #a40011;">{}</span> 
+<p><span style="font-weight: bold; margin: 2px 10px 5px 10px;color: #a40011;">{}</span>
 <span style="font-weight: bold; margin: 2px 10px 5px 10px;color:">{} :</span> </p>
 <p style="margin: 12px 20px 15px 20px;">{}</p>
 """
